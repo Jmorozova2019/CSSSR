cd17481115aaa49b5c85c4a3c3391cde3ad7841d
Jmorozova2019 <Jannamoroz10@ya.ru>
2020-01-23 19:21:29 +0300

    Добавлено логирование в файлы

--- src/test/java/BlogCSSSR/QaEngineerTest.java
+++ src/test/java/BlogCSSSR/QaEngineerTest.java
@@ -1,14 +1,22 @@
 package BlogCSSSR;
+/**
+ * Autor Morozova Zh.A.
+ */
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.logging.Level;
+
 
 public class QaEngineerTest extends WebDriverSettings {
-    @Test
+    @Test(enabled = true)
     public void linkSoftScreenshoterTest() {
+        LOGGER.log(Level.INFO, "\n");
+        LOGGER.log(Level.INFO, "------Cтарт теста валидности сссылки \"Софт для быстрого создания скриншотов\"------");
+
         //Выбрать вкладку НАХОДИТЬ НЕСОВЕРШЕНСТВА
         String xpathFindPerfections = "//a[@href = \"#\" and text() = \"НАХОДИТЬ НЕСОВЕРШЕНСТВА\"]";
         WebElement findPerfections = driver.findElement(By.xpath(xpathFindPerfections));
@@ -19,17 +27,29 @@ public class QaEngineerTest extends WebDriverSettings {
 
         //Проверка, что ссылка открывается в новом окне
         String target = softScreenshoter.getAttribute("target");
-        Assert.assertTrue(target.equals("_blank"), "Ссылка открывается в том же окне");
+        if (!target.equals("_blank")){
+            LOGGER.log(Level.WARNING, "Ссылка открывается в том же окне, должна открываться в новом");
+        }
+        Assert.assertEquals(target, "_blank",
+                "Ссылка открывается в том же окне, должна открываться в новом");
 
         //Проверка адреса ссылки Софт для быстрого создания скриншотов
         String refSoftScreenshoter = softScreenshoter.getAttribute("href");
         String refSoftScreenshoterExpected = "http://monosnap.com/";
-        Assert.assertTrue(refSoftScreenshoter.equals(refSoftScreenshoterExpected),
-                String.format("Ожидаемый адрес ссылки %s, фактический %s", refSoftScreenshoterExpected, refSoftScreenshoter));
+
+        if (!refSoftScreenshoter.equals("refSoftScreenshoterExpected")){
+            LOGGER.log(Level.WARNING, String.format("Ожидаемый адрес ссылки %s, фактический %s", refSoftScreenshoterExpected,
+                    refSoftScreenshoter));
+        }
+        Assert.assertEquals(refSoftScreenshoter, refSoftScreenshoterExpected,
+            String.format("Ожидаемый адрес ссылки %s, фактический %s", refSoftScreenshoterExpected, refSoftScreenshoter));
     }
 
-    @Test
+    @Test(enabled = true)
     public void repeatClickTabTest() {
+        LOGGER.log(Level.INFO, "\n");
+        LOGGER.log(Level.INFO, "------Cтарт теста очистки блока после повторного выбора уже активной ссылки-------");
+
         //Выбрать вкладку повторно кликнуть по заголовку вкладки
         String xpathFindPerfections = "//a[@href = \"#\" and text() = \"СОПРОВОЖДАТЬ ПРОЕКТЫ\"]";
         WebElement findPerfections = driver.findElement(By.xpath(xpathFindPerfections));
@@ -40,6 +60,11 @@ public class QaEngineerTest extends WebDriverSettings {
         String xpathInfoDetailsBlock = "//div[@class = \"info-details\"]";//относится только к первой вкладке
         WebElement infoDetailsBlock = driver.findElement(By.xpath(xpathInfoDetailsBlock));
         String styleInfoDetails = infoDetailsBlock.getAttribute("style");
-        Assert.assertTrue(styleInfoDetails.equals("display: block;"), "После повторного клика блок данных пуст");
+
+        if(!styleInfoDetails.equals("display: block;")){
+            LOGGER.log(Level.WARNING, "После повторного клика на заголовке вкладки блок данных пуст");
         }
+        Assert.assertEquals(styleInfoDetails, "display: block;",
+                "После повторного клика на заголовке вкладки блок данных пуст");
+    }
 }
--- src/test/java/BlogCSSSR/WebDriverSettings.java
+++ src/test/java/BlogCSSSR/WebDriverSettings.java
@@ -1,17 +1,36 @@
 package BlogCSSSR;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 
 public class WebDriverSettings {
     public static String URL_CSSSR = "https://blog.csssr.ru/qa-engineer/";
     public ChromeDriver driver;
 
+    public static Logger LOGGER = Logger.getLogger(WebDriverSettings.class.getName());
+
+    private void setUpLogger(){
+        try {
+            InputStream is = WebDriverSettings.class.getResourceAsStream("/logging.properties");
+            LogManager.getLogManager().readConfiguration(is);
+        } catch (IOException e) {
+            System.err.println("Could not setup logger configuration: " + e.toString());
+        }
+    }
+
     @BeforeClass
     public void setUp() {
+        setUpLogger();
+
         driver = new ChromeDriver();
 
         driver.manage().window().maximize();
